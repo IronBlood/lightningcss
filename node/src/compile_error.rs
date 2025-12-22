9dac04fb2c2f9bdc37d8fd670f7ed29e6154a41d
IronBlood <aplshenyang@gmail.com>
2025-12-22 13:45:35 +0800

    error handling for bundle_async: fix message and attach error location

--- node/src/compile_error.rs
+++ node/src/compile_error.rs
@@ -9,6 +9,8 @@ use lightningcss::{
     PrinterErrorKind,
   },
 };
+use napi::bindgen_prelude::{Function, JsObjectValue, Object, ToNapiValue};
+use napi::Env;
 
 pub enum CompileError<'i, E: std::error::Error> {
   ParseError(Error<ParserError<'i>>),
@@ -76,7 +78,127 @@ impl<'i> From<CompileError<'i, napi::Error>> for napi::Error {
   fn from(e: CompileError<'i, napi::Error>) -> Self {
     match e {
       CompileError::JsError(err) => err,
+      // TODO FIXME
       other => napi::Error::new(napi::Status::GenericFailure, other.to_string()),
+    }
+  }
+}
+
+pub enum CompileErrorOwned {
+  JsError {
+    err: napi::Error,
+    loc: Option<lightningcss::error::ErrorLocation>,
+  },
+  Lightning {
+    message: String,
+    kind: String,
+    loc: Option<lightningcss::error::ErrorLocation>,
+    stage: &'static str,
+  },
+  Other(String),
+}
+
+impl<'i> From<CompileError<'i, napi::Error>> for CompileErrorOwned {
+  fn from(e: CompileError<'i, napi::Error>) -> Self {
+    use lightningcss::error::Error as LcError;
+
+    let message = e.to_string(); // TODO not sure
+    match e {
+      CompileError::JsError(err) => Self::JsError { err, loc: None },
+      CompileError::BundleError(LcError { kind, loc, .. }) => {
+        if let BundleErrorKind::ResolverError(js_err) = kind {
+          return Self::JsError { err: js_err, loc: loc };
+        }
+
+        Self::Lightning {
+          message,
+          kind: format!("{kind}"), // TODO not sure
+          loc,
+          stage: "BundleError",
+        }
+      }
+      CompileError::ParseError(LcError { kind, loc, .. }) => Self::Lightning {
+        message,
+        kind: format!("{kind}"),
+        loc,
+        stage: "ParserError",
+      },
+      CompileError::PrinterError(LcError { kind, loc, .. }) => Self::Lightning {
+        message,
+        kind: format!("{kind}"),
+        loc,
+        stage: "PrinterError",
+      },
+      CompileError::MinifyError(LcError { kind, loc, .. }) => Self::Lightning {
+        message,
+        kind: format!("{kind}"),
+        loc,
+        stage: "MinifyError",
+      },
+      other => CompileErrorOwned::Other(other.to_string()),
+    }
+  }
+}
+
+fn attach_loc(
+  env: &Env,
+  obj: &mut Object,
+  loc: &lightningcss::error::ErrorLocation,
+  code: Option<&str>,
+) -> napi::Result<()> {
+  obj.set("fileName", loc.filename.clone())?;
+  if let Some(code) = code {
+    obj.set("source", code)?;
+  }
+
+  let mut loc_obj = Object::new(env)?;
+  loc_obj.set("line", loc.line + 1)?;
+  loc_obj.set("column", loc.column)?;
+  obj.set("loc", loc_obj)?;
+
+  Ok(())
+}
+
+fn syntax_error_object<'i>(env: &'i Env, message: &str) -> napi::Result<Object<'i>> {
+  let syntax_error = env.get_global()?.get_named_property::<Function<String>>("SyntaxError")?;
+  let obj = syntax_error.new_instance(message.to_string())?;
+  let obj = unsafe { obj.cast::<Object>()? };
+  Ok(obj)
+}
+
+impl CompileErrorOwned {
+  pub fn into_js_error(self, env: Env, code: Option<&str>) -> napi::Result<napi::Error> {
+    match self {
+      CompileErrorOwned::JsError { err, loc } => {
+        let js_err = err.into_unknown(&env)?;
+        if js_err.get_type()? == napi::ValueType::Object {
+          // let mut obj: Object = Object::from(js_err);
+          let mut obj: Object = unsafe { js_err.cast::<Object>()? };
+          if let Some(loc) = loc {
+            attach_loc(&env, &mut obj, &loc, code)?;
+          }
+          let obj = obj.into_unknown(&env)?;
+          Ok(obj.into())
+        } else {
+          Ok(js_err.into())
+        }
+      }
+      CompileErrorOwned::Lightning {
+        message,
+        kind,
+        loc,
+        stage,
+      } => {
+        let mut obj = syntax_error_object(&env, &message)?;
+        if let Some(loc) = loc {
+          attach_loc(&env, &mut obj, &loc, code)?;
+        }
+        obj.set("stage", stage)?;
+        obj.set("kind", kind)?;
+        let unk = obj.into_unknown(&env)?;
+        Ok(unk.into())
+      }
+      CompileErrorOwned::Other(message) => Ok(napi::Error::new(napi::Status::GenericFailure, message)),
     }
   }
 }
