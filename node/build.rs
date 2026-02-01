c60aeac229b84b040d25c1e9307886ce67e1508b
IronBlood <aplshenyang@gmail.com>
2026-02-01 14:54:30 +0800

    ignore napi_build::setup for windows gnu target

--- node/build.rs
+++ node/build.rs
@@ -2,6 +2,11 @@
 extern crate napi_build;
 
 fn main() {
-  #[cfg(all(not(target_arch = "wasm32"), not(all(target_os = "windows", target_env = "gnu"))))]
+  let target = std::env::var("TARGET").unwrap_or_default();
+  if target.ends_with("-pc-windows-gnu") {
+    return;
+  }
+
+  #[cfg(not(target_arch = "wasm32"))]
   napi_build::setup();
 }
